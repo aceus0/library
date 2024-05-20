ec0ff1fe18bf45c75b9105d3c57a548334be2e59
justin <64299580+aceus0@users.noreply.github.com>
2024-05-20 17:16:43 -0400

    did groundwork for book removal

--- script.js
+++ script.js
@@ -2,7 +2,14 @@ const myLibrary = [];
 const librarySection = document.querySelector(`#library`);
 const newBook = document.querySelector(`#new-book`);
 const dialog = document.querySelector("dialog");
+const confirmBtn = document.querySelector("#confirmBtn");
+const cancelBtn = document.querySelector("#cancelBtn");
+const deleteBtn = document.querySelector("#deleteBtn")
 
+let title;
+let author;
+let pages;
+let read;
 
 function Book(title, author, pages, read){
     this.title = title;
@@ -12,12 +19,16 @@ function Book(title, author, pages, read){
 }
 
 function addBookToLibrary(book) {
-    myLibrary.push(book)
+    book.index = myLibrary.length;
+    myLibrary.push(book);
+
 }
 
 function createCard(item) {
     const displayedBook = document.createElement("div");
     displayedBook.classList.add(`book`)
+    const imgPlaceholder = document.createElement("div")
+    imgPlaceholder.classList.add(`img`)
     const bookTitle = document.createElement("div")
     bookTitle.classList.add(`title`)
     const bookAuthor = document.createElement("div")
@@ -26,20 +37,26 @@ function createCard(item) {
     bookPages.classList.add(`pages`)
     const bookRead = document.createElement("div")
     bookRead.classList.add(`read`)
+    const deleteBook = document.createElement("button")
 
+    deleteBook.textContent = `X`
+    displayedBook.setAttribute(`data`, item.index)
     bookTitle.textContent = `Title: ${item.title}`
     bookAuthor.textContent = `By: ${item.author}`
     bookPages.textContent = `Pages: ${item.pages}`
     bookRead.textContent = `Read?: ${item.read}`
-    
+
+    displayedBook.appendChild(deleteBook);
     librarySection.appendChild(displayedBook);
+    displayedBook.appendChild(imgPlaceholder);
     displayedBook.appendChild(bookTitle);
     displayedBook.appendChild(bookAuthor);
     displayedBook.appendChild(bookPages);
     displayedBook.appendChild(bookRead);
+
 }
 
-function displayLibrary(list){
+function populateLibrary(list){
     for (let obj of list){
         createCard(obj);
     }
@@ -52,13 +69,39 @@ const steveJobs = new Book(`Steve Jobs`, `Justin`, `300`, `no`);
 addBookToLibrary(hobbit);
 addBookToLibrary(steveJobs);
 
-displayLibrary(myLibrary)
+populateLibrary(myLibrary);
 
 newBook.addEventListener("click", () => {
     dialog.showModal();
 })
 
+confirmBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    title = document.getElementById('title').value
+    author = document.getElementById('author').value
+    pages = document.getElementById('pages').value
+    read = `no`;
+    if (document.getElementById(`read`).checked) {
+        read = `yes`;
+        console.log(read);
+    }
+
+    let userBook = new Book(title, author, pages, read)
+    console.table(userBook);
+    addBookToLibrary(userBook);
+    createCard(userBook);
+
+    userBook = undefined;
+    dialog.close();
+
+})
 
 
+cancelBtn.addEventListener("click", () => {
+    dialog.close();
+})
 
 
+// deleteBtn.addEventListener("click", (e) => {
+    
+// })
